e5c0d2c760dd7df8b02c98aaa0eca36fe2dd9e29
viteshbava <82492234+viteshbava@users.noreply.github.com>
2025-08-31 16:10:36 +1000

    Adjust columns for blog list

--- src/components/Blog/BlogList.tsx
+++ src/components/Blog/BlogList.tsx
@@ -35,7 +35,7 @@ const BlogList = ({ posts, noPostsText }: BlogListProps) => {
   }
 
   return (
-    <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 md:gap-8'>
+    <div className='grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-6 md:gap-8'>
       {validPosts.map((post) => (
         <div key={post._id} className='flex'>
           <BlogCard {...post} />
