5db05ec70b639ffef5aa4ec1857721cdb45bfd92
viteshbava <82492234+viteshbava@users.noreply.github.com>
2025-09-03 16:30:46 +1000

    Fix img placeholder size

--- src/components/UI/ImgPlaceHolder.tsx
+++ src/components/UI/ImgPlaceHolder.tsx
@@ -4,10 +4,10 @@ import Image from 'next/image';
 const ImgPlaceHolder = () => {
   return (
     <Image
-      src={`data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='600' height='400'><rect width='100%' height='100%' fill='%23ddd'/><g stroke='%23999' stroke-width='3' fill='none'><rect x='100' y='80' width='400' height='240' rx='8' ry='8'/><line x1='120' y1='280' x2='240' y2='160'/><line x1='240' y1='160' x2='360' y2='280'/><circle cx='380' cy='160' r='24'/></g></svg>`}
+      src={`data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='1920' height='1080'><rect width='100%' height='100%' fill='%23ddd'/><g stroke='%23999' stroke-width='6' fill='none'><rect x='320' y='180' width='1280' height='720' rx='24' ry='24'/><line x1='400' y1='900' x2='800' y2='300'/><line x1='800' y1='300' x2='1520' y2='900'/><circle cx='1400' cy='300' r='72'/></g></svg>`}
       alt='No image available'
-      width={600}
-      height={400}
+      width={1920}
+      height={1080}
       className='mx-auto rounded-lg'
       unoptimized
       priority
