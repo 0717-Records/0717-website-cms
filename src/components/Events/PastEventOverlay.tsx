5297c24861879fd080b7e5222674a43a9e1ce6ba
viteshbava <82492234+viteshbava@users.noreply.github.com>
2025-09-07 18:42:42 +1000

    Adjust event past event overlay text style

--- src/components/Events/PastEventOverlay.tsx
+++ src/components/Events/PastEventOverlay.tsx
@@ -6,7 +6,7 @@ interface PastEventOverlayProps {
 
 const PastEventOverlay = ({ text }: PastEventOverlayProps) => (
   <div className='absolute inset-0 flex items-center justify-center z-10'>
-    <div className='text-center text-white font-bold text-body-lg leading-tight px-4'>
+    <div className='text-center text-white leading-tight px-4'>
       {text.split('\n').map((line, index) => (
         <div key={index} className='drop-shadow-lg'>
           {line}
