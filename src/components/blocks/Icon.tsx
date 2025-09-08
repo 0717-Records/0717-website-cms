a055ff74d0231e95b4d576ba69161482ca5bef22
viteshbava <82492234+viteshbava@users.noreply.github.com>
2025-09-08 17:09:09 +1000

    Adjust icon logic

--- src/components/blocks/Icon.tsx
+++ src/components/blocks/Icon.tsx
@@ -8,15 +8,19 @@ interface IconProps extends Omit<IconBlock, '_type' | '_key'> {
 }
 
 const Icon = ({ image, className = '' }: IconProps) => {
-  const imgSrc = image?.asset
-    ? urlFor(image).width(80).height(80).url()
-    : '/images/logo-black-on-transparent.png';
+  const imgSrc = image?.asset ? urlFor(image).url() : '/images/logo-black-on-transparent.png';
   const imgAlt = image?.asset ? image.alt || 'Icon image' : '07:17 Records Logo';
 
   return (
     <div
       className={`relative w-16 h-16 md:w-18 md:h-18 rounded-full bg-brand-gradient flex items-center justify-center ${className}`.trim()}>
-      <Image src={imgSrc} alt={imgAlt} fill sizes='40px' className='object-contain p-1' />
+      <Image
+        src={imgSrc}
+        alt={imgAlt}
+        fill
+        sizes='40px'
+        className={`object-contain ${image?.asset ? 'p-3' : 'p-1'}`}
+      />
     </div>
   );
 };
