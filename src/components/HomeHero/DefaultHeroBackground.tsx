0a4f02fdc19e3a96306cf5de62c55e4a36fb96a6
viteshbava <82492234+viteshbava@users.noreply.github.com>
2025-09-09 22:21:14 +1000

    Adjust hero gradients

--- src/components/HomeHero/DefaultHeroBackground.tsx
+++ src/components/HomeHero/DefaultHeroBackground.tsx
@@ -65,7 +65,7 @@ const DefaultHeroBackground = () => {
       {/* Key decorative circles - minimal and strategic */}
       {/* Large left circle - positioned to peek from bottom-left - ~50vw diameter with min size */}
       <div className='absolute bottom-0 left-1/2 sm:left-0 w-[100vw] h-[100vw] sm:w-[50vw] sm:h-[50vw] rounded-full bg-gradient-to-tr from-[#ffea00]/20 to-[#ffea00]/10 transform -translate-x-1/2 translate-y-1/2 blur-xl' />
-      <div className='absolute bottom-0 left-1/2 sm:left-0 w-[70vw] h-[70vw] sm:w-[35vw] sm:h-[35vw] rounded-full bg-gradient-to-tr from-[#ffea00]/20 to-[#ffea00]/10 transform -translate-x-1/2 translate-y-1/2 blur-sm' />
+      <div className='absolute bottom-0 left-1/2 sm:left-0 w-[70vw] h-[70vw] sm:w-[35vw] sm:h-[35vw] rounded-full bg-gradient-to-tr from-[#ffea00]/10 to-[#ffea00]/05 transform -translate-x-1/2 translate-y-1/2 blur-sm' />
 
       {/* Central accent circle - subtly positioned behind content area - ~25vw diameter with min size */}
       {/* <div className='absolute top-1/2 left-1/2 w-64 h-64 md:w-80 md:h-80 lg:w-[25vw] lg:h-[25vw] rounded-full bg-gradient-to-br from-[#ffea00]/20 to-[#ffea00]/10 transform -translate-y-1/2 -translate-x-1/2 blur-sm' />
