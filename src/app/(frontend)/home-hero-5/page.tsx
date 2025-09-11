944bcf04e3ff3663bd296f419ffcc984e649179d
viteshbava <82492234+viteshbava@users.noreply.github.com>
2025-09-11 19:50:22 +1000

    Add demo home heroes

---
+++ src/app/(frontend)/home-hero-5/page.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import PageBuilder from '@/components/PageBuilder';
+import Hero from '@/components/HomeHero/Hero';
+import { getHomePage, getSiteSettings, getCompanyLinks } from '@/actions';
+import { getAllEvents } from '@/actions/events';
+import { getCollabs } from '@/actions/collabs';
+import { getFavourites } from '@/actions/favourites';
+import type { PAGE_QUERYResult } from '@/sanity/types';
+import Container from '@/components/Layout/Container';
+
+const Page = async () => {
+  const [page, siteSettings, events, collabs, favourites, companyLinks] = await Promise.all([
+    getHomePage(),
+    getSiteSettings(),
+    getAllEvents(),
+    getCollabs(),
+    getFavourites(),
+    getCompanyLinks(),
+  ]);
+
+  if (!page) {
+    return <div>Page not found</div>;
+  }
+
+  return (
+    <>
+      {/* Hero Section */}
+      <Hero
+        heroStyle="background-images"
+        heroTextColor="white"
+        showHeroLogo={false}
+        heroImage={page.heroImage}
+        heroTitle={page.heroTitle}
+        heroSubtitle={page.heroSubtitle}
+        heroCallToActionList={page.heroCallToActionList}
+        heroContentPosition="bottom-right"
+        enableFeaturedItems={false}
+        featuredImages={page.featuredImages}
+        documentId={page._id}
+        documentType={page._type}
+      />
+
+      {/* Additional Page Builder Content */}
+      {page.content && (
+        <Container>
+          <PageBuilder
+            content={page.content as NonNullable<PAGE_QUERYResult>['content']}
+            documentId={page._id}
+            documentType={page._type}
+            siteSettings={
+              siteSettings
+                ? {
+                    companyEmail: siteSettings.companyEmail || undefined,
+                  }
+                : undefined
+            }
+            events={events}
+            collabs={collabs}
+            favourites={favourites}
+            companyLinks={companyLinks}
+            alignment='center'
+          />
+        </Container>
+      )}
+    </>
+  );
+};
+
+export default Page;
