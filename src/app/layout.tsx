4bb721473616bf517404a6368f89575f866a5ea5
viteshbava <82492234+viteshbava@users.noreply.github.com>
2025-09-13 15:26:21 +1000

    Add robots tag to header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,10 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const isProd = process.env.NEXT_PUBLIC_ENV === 'production';
   return (
     <html lang='en'>
+      <head>{!isProd && <meta name='robots' content='noindex, nofollow' />}</head>
       <body>{children}</body>
     </html>
   );
